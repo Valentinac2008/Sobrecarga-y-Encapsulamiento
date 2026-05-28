0d7ccc6f9ee8c4201595b9c4da5cd55a526469df
Valentinac2008 <valentinac613@alu.bue.edu.ar>
2026-05-28 20:54:56 -0300

    Add files via upload

---
+++ Ejercicio bateria/Bateria.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Bateria
+    {
+
+        private int _porcentajeCarga;
+        private int _saludBateria;
+        private bool _conectadoCargador;
+        private bool _modoAhorroEnergia;
+
+
+        private int _acumuladorCarga;
+
+
+        public Bateria()
+        {
+            _porcentajeCarga = 50;
+            _saludBateria = 100;
+            _conectadoCargador = false;
+            _modoAhorroEnergia = false;
+            _acumuladorCarga = 0;
+        }
+
+        public int PorcentajeCarga
+        {
+            get { return _porcentajeCarga; }
+        }
+
+        public int SaludBateria
+        {
+            get { return _saludBateria; }
+        }
+
+        public bool ConectadoCargador
+        {
+            get { return _conectadoCargador; }
+        }
+
+        public bool ModoAhorroEnergia
+        {
+            get { return _modoAhorroEnergia; }
+        }
+
+
+        public string EstadoTexto
+        {
+            get
+            {
+                if (_conectadoCargador)
+                {
+                    return $"CARGANDO - BATERIA: {_porcentajeCarga}%";
+                }
+                else
+                {
+                    return $"BATERIA: {_porcentajeCarga}%";
+                }
+            }
+        }
+            public void AlternarCargador()
+        {
+            _conectadoCargador = !_conectadoCargador;
+        }
+
+        public void ConsumirEnergia()
+        {
+            int consumo = 1;
+
+            if (_modoAhorroEnergia)
+            {
+                consumo = consumo / 2;
+
+                if (consumo == 0)
+                {
+                    consumo = 1;
+                }
+            }
+
+            _porcentajeCarga -= consumo;
+
+            if (_porcentajeCarga < 0)
+            {
+                _porcentajeCarga = 0;
+            }
+
+            VerificarModoAhorro();
+        }
+
+
+        public void ConsumirEnergia(int consumo)
+        {
+            if (_modoAhorroEnergia)
+            {
+                consumo = consumo / 2;
+            }
+
+            _porcentajeCarga -= consumo;
+
+            if (_porcentajeCarga < 0)
+            {
+                _porcentajeCarga = 0;
+            }
+
+            VerificarModoAhorro();
+        }
+
+
+        public void CicloDeCarga()
+        {
+            if (_conectadoCargador)
+            {
+
+                if (_porcentajeCarga < _saludBateria)
+                {
+                    _porcentajeCarga += 10;
+                    _acumuladorCarga += 10;
+
+                    if (_porcentajeCarga > _saludBateria)
+                    {
+                        _porcentajeCarga = _saludBateria;
+                    }
+
+
+                    if (_acumuladorCarga >= 100)
+                    {
+                        _saludBateria--;
+
+                        if (_saludBateria < 0)
+                        {
+                            _saludBateria = 0;
+                        }
+
+                        _acumuladorCarga = 0;
+                    }
+                }
+            }
+
+            VerificarModoAhorro();
+        }
+
+
+        private void VerificarModoAhorro()
+        {
+            if (_porcentajeCarga < 20)
+            {
+                _modoAhorroEnergia = true;
+            }
+
+
+            if (_porcentajeCarga > 50)
+            {
+                _modoAhorroEnergia = false;
+            }
+        }
+    }
+}
---
+++ Ejercicio invernadero/Invernadero.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Invernadero
+    {
+
+        private string _nombreSector;
+        private int _temperaturaActual;
+        private int _humedadSuelo;
+        private bool _sistemaRiegoActivo;
+        private bool _calefaccionActiva;
+        private string _tipoCultivo;
+
+
+        public Invernadero(string nombreSector, string tipoCultivo)
+        {
+            _nombreSector = nombreSector;
+            _tipoCultivo = tipoCultivo.ToUpper();
+
+            _temperaturaActual = 25;
+            _humedadSuelo = 50;
+
+            _sistemaRiegoActivo = false;
+            _calefaccionActiva = false;
+        }
+
+
+        public int TemperaturaActual
+        {
+            get { return _temperaturaActual; }
+        }
+
+        public int HumedadSuelo
+        {
+            get { return _humedadSuelo; }
+        }
+
+        public bool SistemaRiegoActivo
+        {
+            get { return _sistemaRiegoActivo; }
+        }
+
+        public bool CalefaccionActiva
+        {
+            get { return _calefaccionActiva; }
+        }
+
+
+        public string ReporteEstado
+        {
+            get
+            {
+                string alerta = "PARAMETROS OPTIMOS";
+
+                if (_tipoCultivo == "TROPICAL")
+                {
+                    if (_humedadSuelo < 60)
+                    {
+                        alerta = "BAJA HUMEDAD";
+                    }
+                    else if (_temperaturaActual < 20 || _temperaturaActual > 28)
+                    {
+                        alerta = "TEMPERATURA INCORRECTA";
+                    }
+                }
+
+                else if (_tipoCultivo == "DESERTICO")
+                {
+                    if (_humedadSuelo > 20)
+                    {
+                        alerta = "EXCESO DE HUMEDAD";
+                    }
+                    else if (_temperaturaActual < 25 || _temperaturaActual > 35)
+                    {
+                        alerta = "TEMPERATURA INCORRECTA";
+                    }
+                }
+
+                return $"SECTOR: {_nombreSector.ToUpper()} - CULTIVO: {_tipoCultivo} - ALERTA: {alerta}";
+            }
+        }
+
+
+        public void SimularClima()
+        {
+            _humedadSuelo -= 5;
+            _temperaturaActual += 1;
+
+            if (_humedadSuelo < 0)
+            {
+                _humedadSuelo = 0;
+            }
+        }
+
+        public void SimularClima(int humedad, int temperatura)
+        {
+            _humedadSuelo = humedad;
+            _temperaturaActual = temperatura;
+
+            if (_humedadSuelo > 100)
+            {
+                _humedadSuelo = 100;
+            }
+
+            if (_humedadSuelo < 0)
+            {
+                _humedadSuelo = 0;
+            }
+        }
+
+        public void ControlAutomatico()
+        {
+            if (_tipoCultivo == "TROPICAL")
+            {
+
+                if (_humedadSuelo < 60)
+                {
+                    if (_humedadSuelo < 100)
+                    {
+                        _sistemaRiegoActivo = true;
+                    }
+                }
+                else
+                {
+                    _sistemaRiegoActivo = false;
+                }
+
+
+                if (_temperaturaActual < 20)
+                {
+                    if (_temperaturaActual <= 45)
+                    {
+                        _calefaccionActiva = true;
+                    }
+                }
+                else
+                {
+                    _calefaccionActiva = false;
+                }
+            }
+
+            else if (_tipoCultivo == "DESERTICO")
+            {
+
+                if (_humedadSuelo > 20)
+                {
+                    _sistemaRiegoActivo = false;
+                }
+
+
+                if (_temperaturaActual < 25)
+                {
+                    if (_temperaturaActual <= 45)
+                    {
+                        _calefaccionActiva = true;
+                    }
+                }
+                else
+                {
+                    _calefaccionActiva = false;
+                }
+            }
+
+
+            if (_humedadSuelo >= 100)
+            {
+                _sistemaRiegoActivo = false;
+            }
+
+            if (_temperaturaActual > 45)
+            {
+                _calefaccionActiva = false;
+            }
+        }
+    }
+}
