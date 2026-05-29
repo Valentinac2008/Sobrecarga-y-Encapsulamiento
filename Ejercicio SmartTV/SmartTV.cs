ceea95da00ee2901b121948ce2aefccfc4b0bab8
Valentinac2008 <valentinac613@alu.bue.edu.ar>
2026-05-28 21:03:40 -0300

    Update SmartTV.cs

--- Ejercicio SmartTV/SmartTV.cs
+++ Ejercicio SmartTV/SmartTV.cs
@@ -15,60 +15,110 @@ namespace SimuladorSmartTV
         private int _volumen;
         private bool _esPremium;
 
+        public string Marca
+        {
+            get { return _marca; }
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _marca = value.ToUpper();
+                }
+            }
+        }
+
+        public int Pulgadas
+        {
+            get { return _pulgadas; }
+            private set
+            {
+                if (value > 0)
+                {
+                    _pulgadas = value;
+                }
+            }
+        }
+
+        public bool Encendido
+        {
+            get { return _encendido; }
+            private set { _encendido = value; }
+        }
+
+        public int CanalActual
+        {
+            get { return _canalActual; }
+            private set
+            {
+                int canalMaximo = _esPremium ? 500 : 100;
+
+                if (value < 1)
+                {
+                    _canalActual = 1;
+                }
+                else if (value > canalMaximo)
+                {
+                    _canalActual = 1;
+                }
+                else
+                {
+                    _canalActual = value;
+                }
+            }
+        }
+
+        public int Volumen
+        {
+            get { return _volumen; }
+            private set
+            {
+                if (value < 0)
+                {
+                    _volumen = 0;
+                }
+                else if (value > 100)
+                {
+                    _volumen = 100;
+                }
+                else
+                {
+                    _volumen = value;
+                }
+            }
+        }
+
+        public bool EsPremium
+        {
+            get { return _esPremium; }
+            private set { _esPremium = value; }
+        }
+
         public string CodigoConfig
         {
             get
             {
-                string plan = _esPremium ? "PREM" : "STD";
-
-                return $"{_marca.ToUpper()}-{_pulgadas}-{plan}";
+                string plan = EsPremium ? "PREM" : "STD";
+                return $"{Marca}-{Pulgadas}-{plan}";
             }
         }
 
-        public bool Encendido => _encendido;
-        public int CanalActual => _canalActual;
-
-        public string VolumenPantalla
+        public string VolumenTexto
         {
             get
             {
-                return _volumen == 0
-                    ? "MUTE"
-                    : _volumen.ToString();
+                return Volumen == 0 ? "MUTE" : Volumen.ToString();
             }
         }
-     public string Marca{
-            get{return _marca}
-            private set{
-                if(!string.IsNullOrWitheSpace(value)){
-                    _marca = value;
-                }
-            }
-        public int Pulgadas{
-            get{return _pulgadas}
-            private set{
-                _pulgadas = value;
-            }
-        }
-        public bool EsPremium{
-            get{return _esPremium}
-            private set{
-                _esPremium = value;
-            }
-        }
-        public SmartTV(
-            string marca,
-            int pulgadas,
-            bool esPremium
-        )
+
+        public SmartTV(string marca, int pulgadas, bool esPremium)
         {
             Marca = marca;
             Pulgadas = pulgadas;
             EsPremium = esPremium;
 
-            _encendido = false;
-            _canalActual = 1;
-            _volumen = 10;
+            Encendido = false;
+            CanalActual = 1;
+            Volumen = 20;
         }
 
         public void Power()
