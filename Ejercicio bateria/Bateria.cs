38ffb8febd29094fe3b23fe04f599160ba08b46f
Valentinac2008 <valentinac613@alu.bue.edu.ar>
2026-06-30 21:26:54 -0300

    Update Bateria.cs

--- Ejercicio bateria/Bateria.cs
+++ Ejercicio bateria/Bateria.cs
@@ -6,172 +6,87 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    public class Bateria
+  public  class Bateria
+{
+    private int _porcentajeCarga;
+    private int _saludBateria;
+    private bool _conectadoCargador;
+    private bool _modoAhorroEnergia;
+
+    public Bateria(int carga, int salud)
+    {c6
+        this.PorcentajeCarga = carga;
+        this.SaludBateria = salud;
+
+        this.ConectadoCargador = false;
+        this.ModoAhorroEnergia = false;
+    }
+
+    public int PorcentajeCarga
     {
+        get { return _porcentajeCarga; }
+        set { _porcentajeCarga = value; }
+    }
 
-        public class Bateria
+    public int SaludBateria
     {
-        private int _porcentajeCarga;
-        private int _saludBateria;
-        private bool _conectadoCargador;
-        private bool _modoAhorroEnergia;
-        private int _cargaAcumuladaEnCiclo;
+  get { return _saludBateria; }
+        set { _saludBateria = value; }
+    }
 
-        public int PorcentajeCarga
+    public bool ConectadoCargador
+    {
+        get { return _conectadoCargador; }
+        set { _conectadoCargador = value; }
+    }
+
+    public bool ModoAhorroEnergia
+    {
+        get { return _modoAhorroEnergia; }
+        set { _modoAhorroEnergia = value; }
+    }
+
+    public string ESTADO_TEXTO
+    {
+        get
         {
-            get { return _porcentajeCarga; }
-            private set
-            {
-                if (value < 0)
-                {
-                    _porcentajeCarga = 0;
-                }
-                else if (value > 100)
-                {
-                    _porcentajeCarga = 100;
-                }
-                else
-                {
-                    _porcentajeCarga = value;
-                }
-            }
+            return "BATERIA: " + PorcentajeCarga + "%";
         }
+    }
 
-        public int SaludBateria
+    public void AlternarCargador()
+    {
+        ConectadoCargador = !ConectadoCargador;
+    }
+
+    public void ConsumirEnergia()
+    {
+        PorcentajeCarga -= 1;
+    }
+
+    public void ConsumirEnergia(int gasto)
+    {
+        PorcentajeCarga -= gasto;
+    }
+
+    public void CicloDeCarga()
+    {
+        if (ConectadoCargador)
         {
-            get { return _saludBateria; }
-            private set
+            PorcentajeCarga += 10;
+
+            if (PorcentajeCarga > SaludBateria)
             {
-                if (value < 0)
-                {
-                    _saludBateria = 0;
-                }
-                else if (value > 100)
-                {
-                    _saludBateria = 100;
-                }
-                else
-                {
-                    _saludBateria = value;
-                }
-            }
-        }
-
-        public bool ConectadoCargador
-        {
-            get { return _conectadoCargador; }
-            private set { _conectadoCargador = value; }
-        }
-
-        public bool ModoAhorroEnergia
-        {
-            get { return _modoAhorroEnergia; }
-            private set { _modoAhorroEnergia = value; }
-        }
-
-        public string EstadoTexto
-        {
-            get
-            {
-                if (ConectadoCargador)
-                {
-                    return $"CARGANDO - BATERIA: {PorcentajeCarga}%";
-                }
-
-                return $"BATERIA: {PorcentajeCarga}%";
-            }
-        }
-
-        public Bateria()
-        {
-            PorcentajeCarga = 50;
-            SaludBateria = 100;
-            ConectadoCargador = false;
-            ModoAhorroEnergia = false;
-            _cargaAcumuladaEnCiclo = 0;
-        }
-
-        public void AlternarCargador()
-        {
-            ConectadoCargador = !ConectadoCargador;
-        }
-
-        public void ConsumirEnergia()
-        {
-            int consumo = 1;
-
-            if (ModoAhorroEnergia)
-            {
-                consumo = 1 / 2;
-                if (consumo == 0)
-                {
-                    consumo = 1;
-                }
-            }
-
-            PorcentajeCarga -= consumo;
-
-            VerificarModoAhorro();
-        }
-
-        public void ConsumirEnergia(int consumo)
-        {
-            if (consumo < 0)
-            {
-                return;
-            }
-
-            if (ModoAhorroEnergia)
-            {
-                consumo = consumo / 2;
-            }
-
-            PorcentajeCarga -= consumo;
-
-            VerificarModoAhorro();
-        }
-
-        public void CicloDeCarga()
-        {
-            if (!ConectadoCargador)
-            {
-                return;
-            }
-
-            if (PorcentajeCarga < SaludBateria)
-            {
-                int cargaAntes = PorcentajeCarga;
-
-                PorcentajeCarga += 10;
-
-                if (PorcentajeCarga > SaludBateria)
-                {
-                    PorcentajeCarga = SaludBateria;
-                }
-
-                _cargaAcumuladaEnCiclo += PorcentajeCarga - cargaAntes;
-
-                if (_cargaAcumuladaEnCiclo >= 100)
-                {
-                    SaludBateria--;
-                    _cargaAcumuladaEnCiclo = 0;
-                }
-            }
-
-            VerificarModoAhorro();
-        }
-
-        private void VerificarModoAhorro()
-        {
-            if (PorcentajeCarga < 20)
-            {
-                ModoAhorroEnergia = true;
-            }
-
-            if (PorcentajeCarga > 50)
-            {
-                ModoAhorroEnergia = false;
+                PorcentajeCarga = SaludBateria;
             }
         }
     }
+
+    public void Mostrar()
+    {
+        Console.WriteLine(ESTADO_TEXTO);
+        Console.WriteLine("Salud: " + SaludBateria);
+    }
+    }
+}
 }
