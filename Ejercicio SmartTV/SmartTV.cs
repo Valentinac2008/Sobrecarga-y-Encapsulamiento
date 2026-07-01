f3ffb43eda27c851cdae315424005a5fa3813744
Valentinac2008 <valentinac613@alu.bue.edu.ar>
2026-06-30 21:25:32 -0300

    Update SmartTV.cs

--- Ejercicio SmartTV/SmartTV.cs
+++ Ejercicio SmartTV/SmartTV.cs
@@ -6,200 +6,117 @@ using System.Threading.Tasks;
 
 namespace SimuladorSmartTV
 {
-    public class SmartTV
+  class SmartTV
+{
+    private string marca;
+    private int pulgadas;
+    private bool encendido;
+    private int canalActual;
+    private int volumen;
+    private bool esPremium;
+
+    public SmartTV(string marca, int pulgadas, bool esPremium)
+    {this.Marca = marca;
+        this.Pulgadas = pulgadas;
+        this.EsPremium = esPremium;
+
+        this.Encendido = false;
+        this.CanalActual = 1;
+        this.Volumen = 20;
+    }
+
+    public string Marca
     {
-        private string _marca;
-        private int _pulgadas;
-        private bool _encendido;
-        private int _canalActual;
-        private int _volumen;
-        private bool _esPremium;
+        get { return marca; }
+        set { marca = value; }
+    }
 
-        public string Marca
+    public int Pulgadas
+    {
+        get { return pulgadas; }
+        set { pulgadas = value; }
+    }
+
+    public bool Encendido
+    {
+        get { return encendido; }
+        set { encendido = value; }
+    }
+
+    public int CanalActual
+    {
+get { return canalActual; }
+        set { canalActual = value; }
+    }
+
+    public int Volumen
+    {
+        get { return volumen; }
+        set { volumen = value; }
+    }
+
+    public bool EsPremium
+    {
+        get { return esPremium; }
+        set { esPremium = value; }
+    }
+
+    public string CODIGO_CONFIG
+    {
+        get
         {
-            get { return _marca; }
-            private set
+            if (EsPremium)
             {
-                if (!string.IsNullOrWhiteSpace(value))
-                {
-                    _marca = value.ToUpper();
-                }
-            }
-        }
-
-        public int Pulgadas
-        {
-            get { return _pulgadas; }
-            private set
-            {
-                if (value > 0)
-                {
-                    _pulgadas = value;
-                }
-            }
-        }
-
-        public bool Encendido
-        {
-            get { return _encendido; }
-            private set { _encendido = value; }
-        }
-
-        public int CanalActual
-        {
-            get { return _canalActual; }
-            private set
-            {
-                int canalMaximo = _esPremium ? 500 : 100;
-
-                if (value < 1)
-                {
-                    _canalActual = 1;
-                }
-                else if (value > canalMaximo)
-                {
-                    _canalActual = 1;
-                }
-                else
-                {
-                    _canalActual = value;
-                }
-            }
-        }
-
-        public int Volumen
-        {
-            get { return _volumen; }
-            private set
-            {
-                if (value < 0)
-                {
-                    _volumen = 0;
-                }
-                else if (value > 100)
-                {
-                    _volumen = 100;
-                }
-                else
-                {
-                    _volumen = value;
-                }
-            }
-        }
-
-        public bool EsPremium
-        {
-            get { return _esPremium; }
-            private set { _esPremium = value; }
-        }
-
-        public string CodigoConfig
-        {
-            get
-            {
-                string plan = EsPremium ? "PREM" : "STD";
-                return $"{Marca}-{Pulgadas}-{plan}";
-            }
-        }
-
-        public string VolumenTexto
-        {
-            get
-            {
-                return Volumen == 0 ? "MUTE" : Volumen.ToString();
-            }
-        }
-
-        public SmartTV(string marca, int pulgadas, bool esPremium)
-        {
-            Marca = marca;
-            Pulgadas = pulgadas;
-            EsPremium = esPremium;
-
-            Encendido = false;
-            CanalActual = 1;
-            Volumen = 20;
-        }
-
-        public void Power()
-        {
-            _encendido = !_encendido;
-        }
-
-        public void CambiarCanal()
-        {
-            if (!_encendido)
-            {
-                Console.WriteLine(
-                    "La TV está apagada."
-                );
-                return;
-            }
-
-            int canalMaximo =
-                _esPremium ? 500 : 100;
-
-            _canalActual++;
-
-            if (_canalActual > canalMaximo)
-            {
-                _canalActual = 1;
-            }
-        }
-
-        public void CambiarCanal(int canal)
-        {
-            if (!_encendido)
-            {
-                Console.WriteLine(
-                    "La TV está apagada."
-                );
-                return;
-            }
-
-            int canalMaximo =
-                _esPremium ? 500 : 100;
-
-            if (canal >= 1 && canal <= canalMaximo)
-            {
-                _canalActual = canal;
+                return Marca + "-" + Pulgadas + "-PREM";
             }
             else
             {
-                Console.WriteLine(
-                    "Canal fuera de rango."
-                );
-            }
-        }
-
-        public void RegularVolumen(bool subir)
-        {
-            if (!_encendido)
-            {
-                Console.WriteLine(
-                    "La TV está apagada."
-                );
-                return;
-            }
-
-            if (subir)
-            {
-                _volumen += 2;
-
-                if (_volumen > 100)
-                {
-                    _volumen = 100;
-                }
-            }
-            else
-            {
-                _volumen -= 2;
-
-                if (_volumen < 0)
-                {
-                    _volumen = 0;
-                }
+                return Marca + "-" + Pulgadas + "-STD";
             }
         }
     }
+
+    public void Power()
+    {
+        Encendido = !Encendido;
+    }
+
+    public void CambiarCanal()
+    {
+        if (Encendido)
+        {
+            CanalActual++;
+        }
+    }
+
+    public void CambiarCanal(int canal)
+    {
+        if (Encendido)
+        {
+            CanalActual = canal;
+        }
+    }
+
+    public void RegularVolumen(bool subir)
+    {
+        if (Encendido)
+        {
+            if (subir)
+            {
+                Volumen += 2;
+            }
+            else
+            {
+                Volumen -= 2;
+            }
+        }
+    }
+
+    public void MostrarEstado()
+    {
+        Console.WriteLine("Canal: " + CanalActual);
+        Console.WriteLine("Volumen: " + Volumen);
+    }
+    }
+}
 }
